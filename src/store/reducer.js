e86ec3a7e2d050f6d0b68cf3b5c18b272f784956
alopes2 <andre.lopes@exxonmobil.com>
2018-02-21 13:34:46 -0300

    dispatching actions

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,8 +3,38 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    switch(state.type) {
+    console.log('Type: ', action.type);
+    switch(action.type) {
+        case 'INCREMENT':
+            let updatedCounter = state.counter;
+            updatedCounter++;
+            return {
+                ...state,
+                counter: updatedCounter
+            };
+        case 'DECREMENT':
+            let decrementedCounter = state.counter;
+            decrementedCounter--;
+            return {
+                ...state,
+                counter: decrementedCounter
+            };
+        case 'ADD':
+            let addedCounter = state.counter;
+            addedCounter += action.value;
+            return {
+                ...state,
+                counter: addedCounter
+            };
+        case 'SUBTRACT':
+            let subtractedCounter = state.counter;
+            subtractedCounter -= action.value;
+            return {
+                ...state,
+                counter: subtractedCounter
+            };
         default: 
+            console.log('Default')
             return state;
     }
 }
