999503b50d395962bf59cce550b153fe1775df27
alopes2 <andre.lopes@exxonmobil.com>
2018-02-21 13:10:14 -0300

    creating reducer and store

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    counter: 0
+}
+
+const reducer = (state = initialState, action) => {
+    switch(state.type) {
+        default: 
+            return state;
+    }
+}
+
+export default reducer;
